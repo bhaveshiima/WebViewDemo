0e092c838edcde1844f57bc3bd0aa91fa74bebd7
bhavesh <bhavesh.bece.13@gmail.com>
2018-08-04 17:22:06 +0530

    Display the Dialog message when page is loading..

--- app/src/main/java/vsl/bhavesh/webviewdemo/MainActivity.kt
+++ app/src/main/java/vsl/bhavesh/webviewdemo/MainActivity.kt
@@ -1,7 +1,10 @@
 package vsl.bhavesh.webviewdemo
 
+import android.app.ProgressDialog
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,8 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var pDial = ProgressDialog(this) // declare variable for progress dialog
+        pDial.setMessage("Please wait.. Page is loading...!")
+        
+        //wview.webViewClient = WebViewClient() //set on webview
+         wview.webViewClient = object : WebViewClient() {
 
-        wview.webViewClient = WebViewClient() //set on webview
+             // Before loading the page this method called
+             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                 super.onPageStarted(view, url, favicon)
+                 pDial.show()
+             }
+
+             // After loading the page this method called
+             override fun onPageFinished(view: WebView?, url: String?) {
+                 super.onPageFinished(view, url)
+                 pDial.dismiss()
+             }
+         }
+
 
         //enable javascript in webview - youtube or banner in mostly website [ START ]
         wview.settings.javaScriptEnabled = true  // Enable javascript
